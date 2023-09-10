186cb7e9af129c078dc64ac32e5378bfbb4e980a
rakibahmeddev <rakibahmed.dev@gmail.com>
2023-09-10 21:24:34 +0600

    data function and it returns a object and we can show object data in html by using double curli-bracket git push

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
     //data, function, click event, template etc we can use
 
-    template: "<h1> Hello from template</h1>"
+    // template: "<h1> Hello from template</h1>"
+
+    data(){
+        return {
+            // variable, array, object function etc thakte pare 
+            title: 'This is title',
+        }
+    }
 });
 
 
